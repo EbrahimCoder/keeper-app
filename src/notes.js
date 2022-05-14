63500b239f1e7fffa952ea90e2f10163d939a128
EbrahimCoder <105370028+EbrahimCoder@users.noreply.github.com>
2022-05-14 17:29:44 +0530

    Completed Challenge 2.

--- src/notes.js
+++ src/notes.js
@@ -22,5 +22,12 @@ const notes = [
     title: "Hardware vs. Software",
     content:
       "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software."
+  },
+  {
+    key: 5,
+    title: "Reading",
+    content: "That is the way to go"
   }
 ];
+
+export default notes;
